3f22f1d065830b0f793b1c20cb579113d372da91
Paul Belanger <paul.belanger@polybeacon.com>
2012-09-30 17:53:52 -0400

    Rework general
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- puppet/modules/astricon12/manifests/general.pp
+++ puppet/modules/astricon12/manifests/general.pp
@@ -1,7 +1,7 @@
 #
-# puppet-modules: The Kickstand Project
+# kickstandproject-astricon12: The Kickstand Project
 #
-# Copyright (C) 2011, Polybeacon, Inc.
+# Copyright (C) 2012, PolyBeacon, Inc.
 #
 # Paul Belanger <paul.belanger@polybeacon.com>
 #
@@ -16,41 +16,14 @@
 # file at the top of the source tree.
 #
 class astricon12::general(
-    $environment    = 'production',
-    $puppet_server  = 'puppet-01-prod.polybeacon.com'
+  $environment    = 'production',
+  $puppet_server  = 'puppet-01-prod.polybeacon.com'
 ) {
-    $nagios_nsca_server = 'nagios-01-test.polybeacon.lan'
-    $timezone_zoneinfo = 'America/Toronto'
+  $timezone_zoneinfo = 'America/Toronto'
 
-    class { 'apt::client': }
-    class { 'ntp::server': }
-    class { 'nsca::client':
-        server => 'nagios-01-test.polybeacon.lan',
-    }
-    class { 'timezone::client': }
-    class { 'puppet::client':
-        enable  => false,
-        options => {
-            'environment'   => $environment,
-            'server'        => $puppet_server,
-        }
-    }
-    class { 'monitor::client':
-        server => 'nagios-01-test.polybeacon.lan',
-    }
-
-    class { 'dhcp::client': }
-
-    nagios::function::service::nsca { 'check_all_disks':
-        check_command   => 'check_all_disks!20%!10%',
-        description     => 'Disk Space',
-        ensure          => present,
-        server          => $nagios_nsca_server,
-    }
-
-    nagios::function::nsca::host { $::fqdn:
-        server  => $nagios_nsca_server,
-    }
+  class { 'apt::client': }
+  class { 'ntp::server': }
+  class { 'timezone::client': }
 }
 
-# vim:sw=4:ts=4:expandtab:textwidth=79
+# vim:sw=2:ts=2:expandtab:textwidth=79
